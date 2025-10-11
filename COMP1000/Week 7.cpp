965e541cac6ba4fb980bb39cf03556aeb5088d44
Emily <galaxyfox621@gmail.com>
2025-10-11 21:13:50 +0100

    Week 7 prerequisites

---
+++ COMP1000/Week 7.cpp
@@ -0,0 +1,12 @@
+#include <cerrno>
+#include <cmath>
+#include <cstdlib>
+#include <cstring>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <vector>
+
+int main() {
+    return 0;
+}
